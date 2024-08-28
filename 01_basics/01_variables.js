280c1c88f593f12b26d040025395e07e387f33f1
Aayush Sharma <saayush022@gmail.com>
2024-08-28 11:13:52 +0000

    variables definition

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 12345
+let accountEmail = "dssidoihoid@google.com";
+var accountPassword= "54644"
+let accountState;
+accountCity = "Meerut"
+accountEmail = "saayush022@goolge.xom"
+accountPassword = "12122"
+accountCity = "Delhi"
+console.table ([accountId, accountEmail , accountPassword , accountCity, accountState])
