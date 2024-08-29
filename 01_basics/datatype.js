c365deaa1d9bd0720d5ac1ae97bb9917ec0a2115
Aayush Sharma <saayush022@gmail.com>
2024-08-29 12:36:06 +0000

    learnedConversionofdatatypes

---
+++ 01_basics/datatype.js
@@ -0,0 +1,16 @@
+/*let score = "Aayush"
+console.log (typeof score)
+let valurInNumber = Number(score)
+console.log (typeof valurInNumber);
+console.log (valurInNumber)*/
+
+
+let isLoggedin = 1
+let booleanIsLoggedin = Boolean(isLoggedin)
+console.log( booleanIsLoggedin);
+
+let stringValue= 25
+
+let stringValPass = String (stringValue)
+console.log (stringValPass)
+console.log (typeof stringValPass)
