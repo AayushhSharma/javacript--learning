90e1ec1a4348e79d482ab6d59911e7e325835704
Aayush Sharma <saayush022@gmail.com>
2024-09-03 14:24:56 +0000

    strings

---
+++ 01_basics/Strings.js
@@ -0,0 +1,6 @@
+
+const name = "Aayush"
+const repo = 50
+
+console.log (name + repo + "Valkue")
+console.log (`Hello my name is ${name} and my github repo count is ${repo} `)
---
+++ 01_basics/stack.js
@@ -0,0 +1,12 @@
+let myName = "AayushSharma"
+
+let anotherName = myName
+console.log(anotherName)
+let user ={
+    email : "aayu@gmail.com",
+    UPi :"userby@yl"
+}
+let user2 = user
+user2.email = "aayus@google.com"
+console.log (user.email)
+console.log (user2.email)
