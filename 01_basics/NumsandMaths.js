eec413f4f7aece36f9ee78593ac5179a74d436d1
Aayush Sharma <saayush022@gmail.com>
2024-09-06 14:26:59 +0000

    Learned Maths and nums

---
+++ 01_basics/NumsandMaths.js
@@ -0,0 +1,16 @@
+const score = 400
+// console.log (score)
+const balance = new Number (56)
+
+// console.log (balance)
+
+// console.log (balance.toString().length)
+// console.log (balance.toFixed(2))
+const ntherWorld = 123.987
+// console.log (ntherWorld.toPrecision(4))
+const hundred = 10000000
+// console.log (hundred.toLocaleString('en-In'))
+//++++++++++++Maths+++++++++++++++//
+const max =20
+const min = 10
+console.log (Math.floor(Math.random()* (max-min+1))+min)
--- 01_basics/Strings.js
+++ 01_basics/Strings.js
@@ -2,5 +2,11 @@
 const name = "Aayush"
 const repo = 50
 
-console.log (name + repo + "Valkue")
-console.log (`Hello my name is ${name} and my github repo count is ${repo} `)
+// console.log (name + repo + "Valkue")
+// console.log (`Hello my name is ${name} and my github repo count is ${repo} `)
+const url  = "https://aayushsharma/aayussharmas840"
+
+
+
+console.log(url.replace ('840', '-'))
+console.log(url.includes('alok'))
