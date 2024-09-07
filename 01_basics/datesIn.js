12c3dfb591119799493d09c1c31c661098efcf51
Aayush Sharma <saayush022@gmail.com>
2024-09-07 11:52:52 +0000

    learned arrays

---
+++ 01_basics/datesIn.js
@@ -0,0 +1,9 @@
+// let myDate = new Date ()
+// console.log (myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log (myDate.toLocaleDateString())
+// console.log (myDate.toLocaleString())
+
+let myDate = new Date  (2024 ,3,16,5,4)
+console.log (myDate.toLocaleString())
