dacfe7c724792d88eb8291c949f39d0b0e7e41fb
Aayush Sharma <saayush022@gmail.com>
2024-09-25 14:05:33 +0000

    for loop

---
+++ 05_iterations/for.js
@@ -0,0 +1,28 @@
+for (let i = 0; i <=10; i++) {
+    const element = i;
+    if (element==5){
+        // console.log("5 is the best numbber");
+        
+    }
+    // console.log(element);
+    
+    
+}
+for (let i = 0; i <=10; i++) {
+    // console.log(`Outer loop value:${i}`);
+    
+    for (let j = 0; j <=10; j++) {
+    //  console.log(`inner loop value ${j} and outer loop value${i}`);
+    //  console.log(i + '*'+ j + '-' + i*j);
+     
+        
+    }
+    
+}
+let myArray = [ "flash" , "Batman" , "jetblack", "Umangasour"]
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+    
+    
+}
